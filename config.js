b477482dc722f65394f54e3ef8e6d5e0bdf835f4
Iron Man <narendrannirmal6753@gmail.com>
2024-10-18 07:40:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironmanhax@fansonly.com",
+  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
   PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
