61ee3a381600824a0ffdc00e617135492d4682ba
wanzofc only <zanssxploit@gmail.com>
2025-03-21 00:06:39 +0700

    Update and rename config.js to login.html

--- config.js
+++
@@ -1,8 +0,0 @@
-// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
-module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "berlianawan498@gmail.com",
-  PASS: process.env.PASS || "olre djtq lzyu oaxg",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
-}
